cc040533b67d1fed3e4a9b7a204e5f78900a4a01
Nilson Melo <nildevstudio@outlook.com>
2021-01-25 10:50:47 -0300

    camada de domínio (POCO)

---
+++ NilDevStudio.Musicas.Dominio/Album.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NilDevStudio.Musicas.Dominio
+{
+    class Album
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public int Ano { get; set; }
+        public String Observacoes { get; set; }
+    }
+}
