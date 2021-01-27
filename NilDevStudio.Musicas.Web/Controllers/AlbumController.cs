f66ab2770883e9893b8af1e91820af65f44ce202
Nilson Melo <nildevstudio@outlook.com>
2021-01-27 12:59:05 -0300

    No conforto do scaffold, AlbumController,Views

---
+++ NilDevStudio.Musicas.Web/Controllers/AlbumController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NilDevStudio.Musicas.AcessoDados.Entity.Context;
+using NilDevStudio.Musicas.Dominio;
+
+namespace NilDevStudio.Musicas.Web.Controllers
+{
+    public class AlbumController : Controller
+    {
+        private MusicasDbContext db = new MusicasDbContext();
+
+        // GET: Album
+        public ActionResult Index()
+        {
+            return View(db.Albuns.ToList());
+        }
+
+        // GET: Album/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = db.Albuns.Find(id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return View(album);
+        }
+
+        // GET: Album/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Album/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nome,Ano,Observacoes")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Albuns.Add(album);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(album);
+        }
+
+        // GET: Album/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = db.Albuns.Find(id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return View(album);
+        }
+
+        // POST: Album/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nome,Ano,Observacoes")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(album).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(album);
+        }
+
+        // GET: Album/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = db.Albuns.Find(id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return View(album);
+        }
+
+        // POST: Album/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Album album = db.Albuns.Find(id);
+            db.Albuns.Remove(album);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
